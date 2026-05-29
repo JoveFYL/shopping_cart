134a62671c6dd8769f4d3dcbc579ca4d8d95e6a2
Jove <jovefong62@gmail.com>
2026-05-29 13:04:03 +0800

    style: change color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from './components/Header';
 import styles from './App.module.css';
+import { CartProvider } from "./CartContext";
 
 function App() {
-
   return (
-    <>
+    <CartProvider>
       <div className={styles.container}>
         <Header />
         <div className={styles.content}>
           <Outlet />
         </div>
       </div>
-    </>
+    </CartProvider>
   )
 }
 
--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,10 +1,17 @@
 import styles from './Shop.module.css';
+import useProducts from '../hooks/useProducts';
+import Card from './Card';
 
 export default function Shop() {
+    const { products, loading, error } = useProducts();
     return (
         <>
             <div className={styles.container}>
-                <h1>Shop</h1>
+                {
+                    products.map(product => {
+                        return <Card product={product} key={product.id} />
+                    })
+                }
             </div>
         </>
     )
